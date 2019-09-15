654703ffe5a677a9b584f5c808e9b548e35029f9
Jun Hyeok Jung <9913103+CheeseStick@users.noreply.github.com>
2019-09-15 15:23:31 +1200

    Project init.

---
+++ RandomUser/ViewControllers/NC_Main.swift
@@ -0,0 +1,30 @@
+//
+//  NC_Main.swift
+//  RandomUser
+//
+//  Created by Jun Jung on 9/15/19.
+//  Copyright © 2019 JunJung. All rights reserved.
+//
+
+import UIKit
+
+class NC_Main: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
